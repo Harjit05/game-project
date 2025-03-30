274dd0a7c071395f997716ed5894f605da68e6f6
Harjit Kaur <harjitkaurkhalsa1@gmail.com>
2025-03-30 12:51:55 +0530

    links

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,33 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
-  //  return <Button colorScheme="blue">Click me</Button>
+ const[selectedGenre, setSelectedGenre]= useState<Genre | null>(null);
+
   return(
     <Grid templateAreas={{
       base:`"nav" "main"`,
       lg:`"nav nav" "aside main"`
-    }}>
+    }}
+    templateColumns={{
+      base:'1fr',
+      lg:'200px 1fr'
+    }}
+    >
       <GridItem area="nav"  >
         <NavBar />
       </GridItem>
       {/* <Show >  */}
-      <GridItem area="aside" >
-        Aside
+      <GridItem area="aside" paddingX={5}>
+        <GenreList onSelectGenre={(genre)=>setSelectedGenre(genre)}/>
       </GridItem>
       {/* </Show> */}
       <GridItem area="main" >
-        <GameGrid/>
+        <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
   );
